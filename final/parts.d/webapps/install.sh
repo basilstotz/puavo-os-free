5d598c0a7fc297903bf6d93ac97ea1867c8382d3
Basil Stotz <stotz.basil@opinsys-WvbtnHB7>
2021-02-14 19:59:31 +0100

    fix icons

--- final/parts.d/webapps/install.sh
+++ final/parts.d/webapps/install.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 test -d ./tree && rm -r ./tree
 mkdir -p ./tree/usr/share/applications/
 mkdir -p ./tree/usr/local/bin/
-mkdir -p ./tree/usr/share/pixmamps
+mkdir -p ./tree/usr/share/pixmaps
 
 INPUT="./webapps.csv"
 while IFS= read -r LINE; do
@@ -15,15 +15,23 @@ while IFS= read -r LINE; do
     URL=$(echo $LINE|cut -d\; -f3)
 
     #echo " name $NAME text $TEXT url $URL"
+
+    ICON=$(find ./icons/  -name $NAME.png)
+    test -n "$ICON" || ICON="./icons/default.png"
+    cp $ICON ./tree/usr/share/pixmaps/
+    
+    BIN=$(find ./bin/ -name $NAME)
+    if test -z "$BIN";then
 cat <<EOF > ./tree/usr/local/bin/$NAME
 #!/bin/sh
 chromium --app $URL
 exit $?
 EOF
+    else
+	cp ./bin/$NAME > ./tree/usr/local/bin/$NAME
+    fi
     chmod +x ./tree/usr/local/bin/$NAME
 
-    ICON=$(find ./icons/  -name $NAME.png)
-    test -n "$ICON" || ICON="jitsi.png"
     
 cat <<EOF > ./tree/usr/share/applications/$NAME.desktop
 [Desktop Entry]
