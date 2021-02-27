c80d9277b3396de226e7d44a70b4322b1fa2405e
Basil Stotz <stotz.basil@opinsys-WvbtnHB7>
2021-02-27 19:51:30 +0100

    small fix

--- final/bin.d/lookup-image-server-by-dns-patch.sh
+++ final/bin.d/lookup-image-server-by-dns-patch.sh
@@ -9,7 +9,7 @@ test -f $bin.ori || cp $bin $bin.ori
 
 
 
-rep='a=$(avahi-browse -artp|grep "puavo bootserver" |grep =|cut -d";" -f8|uniq);if test -n "$a";then image_server="${a}:872";else exit 1;fi'
+rep='a=$(avahi-browse -artp|grep "puavo bootserver" |grep =|cut -d";" -f8|head -n1);if test -n "$a";then image_server="${a}:872";else exit 1;fi'
 
 
 sed $bin.ori -e "s@exit 1@$rep@" > $bin
