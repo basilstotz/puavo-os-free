09737e558d2a95e5a9f58e02d9444d5c89b24d2b
Basil Stotz <stotz.basil@opinsys-WvbtnHB7>
2021-03-09 15:10:40 +0100

    *

--- final/pre.d/patch-image-conf.sh
+++ final/pre.d/patch-image-conf.sh
@@ -3,13 +3,18 @@
 CHROOT=$1
 CURRENT=$2
 
-LIST="puavo.support.new_bugreport_url puavo.support.image_changelog_url puavo.boot.plymouth.theme puavo.greeter.logo puavo.desktop.vendor.logo puavo.desktop.\
-keyboard.layout puavo.time.timezone puavo.desktop.keyboard.variant puavo.l10n.locale"
+LIST="puavo.support.new_bugreport_url puavo.support.image_changelog_url puavo.boot.plymouth.theme puavo.greeter.logo puavo.desktop.vendor.logo puavo.desktop.keyboard.layout puavo.time.timezone puavo.desktop.keyboard.variant puavo.l10n.locale puavo.xsessions.alternative_languages"
+
+                
 
 patch_image_conf(){
 echo "{"
 for K in $LIST;do
+    if test "$K" = "puavo.xsessions.alternative_languages";then
+	echo "\"$K\": \"de fr fi en sv\","
+    else
         echo "\"$K\": \"$(puavo-conf $K)\","
+    fi
 done
 while IFS= read -r line;do
     #echo "--->$line"                                                                                                                                         
